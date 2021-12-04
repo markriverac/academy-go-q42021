47fc98b938aa9b5f28d60ef40351a82d3d4acb80
Mark Rivera <mark.rivera@wizeline.com>
2021-12-03 22:44:32 -0600

    Connect to Pokemon API

--- pokemon/entity.go
+++ pokemon/entity.go
@@ -1,16 +1,23 @@
 package pokemon
 
 type Pokemon struct {
-	Id   string `json:"Id"`
 	Name string `json:"Name"`
+	Url  string `json:"Url"`
+}
+
+type PokemonResponse struct {
+	Count    int       `json:"Count"`
+	Next     string    `json:"Next"`
+	Previous string    `json:"Previous`
+	Results  []Pokemon `json:"Results"`
 }
 
 func ParseToPokemon(pokemonLines [][]string) []Pokemon {
 	var pokemonList []Pokemon
 	for _, line := range pokemonLines {
 		newPokemon := Pokemon{
-			Id:   line[0],
-			Name: line[1],
+			Name: line[0],
+			Url:  line[1],
 		}
 		pokemonList = append(pokemonList, newPokemon)
 	}
